8ccfd142e31b52dac9c56c53155115d2f34cb70c
Tyler Stephen <tyler.stephen@gmail.com>
2017-01-16 17:25:28 -0800

    file reading!

--- app.go
+++ app.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/op/go-logging"
 )
@@ -11,15 +14,57 @@ var (
 	log = logging.MustGetLogger("audit")
 )
 
+type command struct {
+	ID     int
+	name   string
+	userID string
+	args   []string
+}
+
 func main() {
 	initLogging()
 
-	log.Debugf("Here's a formatted string --> %s <---", "butts")
-	log.Info("info")
-	log.Notice("notice")
-	log.Warning("warning")
-	log.Error("error")
-	log.Critical("critical")
+	// Find the workload file and open it
+	// -  Read each line and:
+	// -    parse the command
+	// -    execute it
+	// -    log it
+
+	// naïvely assume this is the workload file
+	infile := os.Args[1]
+	if _, err := os.Stat(infile); os.IsNotExist(err) {
+		// can't find input; bail!
+		log.Critical(err.Error())
+		os.Exit(1)
+	} else if err != nil {
+		log.Error(err.Error())
+	}
+
+	// open the file
+	file, err := os.Open(infile)
+	if err != nil {
+		log.Critical(err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	log.Debugf("Opened %s", file.Name())
+
+	// process all lines
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		cmd := parseCommand(scanner.Text())
+		if err := executeCommand(cmd); err != nil {
+			// if it fails, log and continue on
+			log.Errorf("Command execution error! cmd # %3d message: %s", cmd.ID, err.Error())
+		}
+	}
+
+	// catch read errors
+	if err := scanner.Err(); err != nil {
+		log.Critical(err.Error())
+		os.Exit(1)
+	}
 }
 
 func initLogging() {
@@ -30,10 +75,43 @@ func initLogging() {
 	consoleBackend := logging.NewLogBackend(os.Stdout, "", 0)
 
 	var consoleFormat = logging.MustStringFormatter(
-		`%{time:15:04:05.000} %{color}▶ %{level:8s}%{color:reset} %{id:03x} %{shortfile} %{message}`,
+		`%{time:15:04:05.000} %{color}▶ %{level:8s}%{color:reset} %{id:03d} %{shortfile} %{message}`,
 	)
 
 	consoleBackendFormatter := logging.NewBackendFormatter(consoleBackend, consoleFormat)
 
 	logging.SetBackend(consoleBackendFormatter)
+}
+
+func parseCommand(s string) command {
+	log.Debugf("Parsing: %s", s)
+
+	// Convert to a proper .csv, then parse
+	// change `[100] STUFF,...` -> `100,STUFF,...`
+	csv := strings.Replace(s, "[", "", 1)
+	csv = strings.Replace(csv, "] ", ",", 1)
+
+	// commands have mystery spaces?!
+	csv = strings.Replace(csv, " ", "", -1)
+
+	// Break it apart
+	parts := strings.Split(csv, ",")
+
+	// Almost all commands will follow this format
+	// TODO: Deal with the final "DUMPLOG,./testLOG"
+	ID, _ := strconv.Atoi(parts[0])
+	parsed := command{
+		ID:     ID,
+		name:   parts[1],
+		userID: parts[2],
+		args:   parts[3:],
+	}
+
+	log.Debugf("Parsed as: %+v", parsed)
+
+	return parsed
+}
+
+func executeCommand(cmd command) error {
+	return nil
 }
