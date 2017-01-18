4918fc4f84e6a26ebcca32ae68b9fc684cd7aeac
Tyler Stephen <tyler.stephen@gmail.com>
2017-01-18 15:19:06 -0800

    ADD ✅ QUOTE ✅ Console logging 👍  (#1)
    * Logging ✅
    
    * Isolate logger init
    
    * file reading!
    
    * Very basic command handler pipeline
    
    * Control log level from command args
    
    * const command names
    
    * babby’s first export
    
    * BUY ✅
    
    * Saner log level passing. No one likes ints.
    
    * Quoteserver shim and some log level tinkering
    
    * Gets quotes from the server!
    
    * Return a Quote struct
    
    * QUOTE ✅
    
    * Include local files as relative path, not separate packages
    
    - Won’t build without this

---
+++ app.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/op/go-logging"
+
+	"./accounts"
+	"./commands"
+	"./quotecache"
+)
+
+// Globals
+var (
+	log = logging.MustGetLogger("audit")
+
+	logLevel = flag.String("loglevel", "WARNING", "CRITICAL, ERROR, WARNING,  NOTICE, INFO, DEBUG")
+
+	accountStore = accounts.NewAccountStore()
+)
+
+// I suck at namespacing and don't want to type commands.Command over and over
+type command commands.Command
+
+func main() {
+	flag.Parse()
+	initLogging()
+
+	// Find the workload file and open it
+	// -  Read each line and:
+	// -    parse the command
+	// -    execute it
+	// -    log it
+
+	// naïvely assume this is the workload file
+	infile := flag.Arg(0)
+	if _, err := os.Stat(infile); os.IsNotExist(err) {
+		// can't find input; bail!
+		log.Critical(err.Error())
+		os.Exit(1)
+	} else if err != nil {
+		log.Error(err.Error())
+	}
+
+	// open the file
+	file, err := os.Open(infile)
+	if err != nil {
+		log.Critical(err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	log.Debugf("Opened %s", file.Name())
+
+	// process all lines
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		cmd := parseCommand(scanner.Text())
+		if err := executeCommand(cmd); err != nil {
+			// if it fails, log and continue on
+			log.Errorf("Command execution error! cmd # %3d message: %s", cmd.ID, err.Error())
+		}
+	}
+
+	// catch read errors
+	if err := scanner.Err(); err != nil {
+		log.Critical(err.Error())
+		os.Exit(1)
+	}
+
+	log.Debugf("Done!")
+}
+
+func initLogging() {
+	// TODO: DONE 1. Make a logger that outputs to console
+	// TODO: DONE 2. Set variable output levels based on runtime flag
+	// TODO: 3. Log stuff into a file
+
+	// Create a default backend
+	consoleBackend := logging.NewLogBackend(os.Stdout, "", 0)
+
+	// Add output formatting
+	var consoleFormat = logging.MustStringFormatter(
+		`%{time:15:04:05.000} %{color}▶ %{level:8s}%{color:reset} %{id:03d} %{shortfile} %{message}`,
+	)
+	consoleBackendFormatted := logging.NewBackendFormatter(consoleBackend, consoleFormat)
+
+	// Add leveled logging
+	level, err := logging.LogLevel(*logLevel)
+	if err != nil {
+		fmt.Println("Bad log level. Using default.") // ERROR
+	}
+	consoleBackendFormattedAndLeveled := logging.AddModuleLevel(consoleBackendFormatted)
+	consoleBackendFormattedAndLeveled.SetLevel(level, "")
+
+	// Attach the backend
+	logging.SetBackend(consoleBackendFormattedAndLeveled)
+}
+
+func parseCommand(s string) command {
+	log.Debugf("Parsing: %s", s)
+
+	// Convert to a proper .csv, then parse
+	// change `[100] STUFF,...` -> `100,STUFF,...`
+	csv := strings.Replace(s, "[", "", 1)
+	csv = strings.Replace(csv, "] ", ",", 1)
+
+	// commands have mystery spaces?!
+	csv = strings.Replace(csv, " ", "", -1)
+
+	// Break it apart
+	parts := strings.Split(csv, ",")
+
+	// Almost all commands will follow this format
+	// TODO: Deal with the final "DUMPLOG,./testLOG"
+	ID, _ := strconv.Atoi(parts[0])
+	name, _ := commands.ToCommandType(parts[1])
+	parsed := command{
+		ID:     ID,
+		Name:   name,
+		UserID: parts[2],
+		Args:   parts[3:],
+	}
+
+	log.Debugf("Parsed as: %+v", parsed)
+
+	return parsed
+}
+
+func executeCommand(cmd command) error {
+	// Each command will return true if everything is okay, false if error
+	var status bool
+
+	// Filter based on the "enum" of command names
+	switch cmd.Name {
+	case commands.Add:
+		status = executeAdd(cmd)
+		break
+	case commands.Quote:
+		status = executeQuote(cmd)
+		break
+	default:
+		log.Warningf("Not implemented: %s", cmd.Name)
+		return nil
+	}
+
+	// report our status
+	if status {
+		log.Debugf("Finished command %d", cmd.ID)
+	} else {
+		log.Debugf("Finished command %d with errors", cmd.ID)
+	}
+
+	return nil
+}
+
+// Add funds to the user's account
+func executeAdd(cmd command) bool {
+	// Finish parsing the rest of the command.
+	// ADD should have an amount passed
+
+	// Sanitize the command
+	if len(cmd.Args) != 1 {
+		// too many
+		log.Errorf("Wrong number of commands: `%s`", cmd.Args)
+		return false
+	} else if cmd.Args[0] == "" {
+		// missing
+		log.Error("No amount passed to ADD")
+		return false
+	}
+
+	// Convert to a float
+	amount, err := strconv.ParseFloat(cmd.Args[0], 64)
+	if err != nil {
+		// Bail on parse failure
+		log.Error(err.Error())
+		return false
+	}
+
+	// Create an account if the user needs one
+	if !accountStore.HasAccount(cmd.UserID) {
+		log.Noticef("Creating account for %s", cmd.UserID)
+		if err := accountStore.CreateAccount(cmd.UserID); err != nil {
+			log.Error(err.Error())
+			return false
+		}
+	}
+
+	// Add the amount
+	log.Infof("Adding %.2f to %s", amount, cmd.UserID)
+	if err := accountStore.Accounts[cmd.UserID].AddFunds(amount); err != nil {
+		log.Error(err.Error())
+		return false
+	}
+	balance := accountStore.Accounts[cmd.UserID].Balance
+	log.Infof("New balance for %s is %.2f", cmd.UserID, balance)
+
+	return true
+}
+
+// Gets a quote from the quoteserver
+func executeQuote(cmd command) bool {
+	// Get the stock from the command
+	stock := cmd.Args[0]
+	if stock == "" {
+		log.Error("No stock passed to QUOTE")
+		return false
+	}
+
+	// get a quote for the stock. (cache will determine if a fresh one is needed)
+	quote, err := quotecache.GetQuote(cmd.UserID, stock)
+	if err != nil {
+		log.Error(err.Error())
+		return false
+	}
+
+	log.Noticef("Got quote: %+v", quote)
+	// send the quote to the user
+	return true
+}
---
+++ quotecache/quotecache.go
@@ -0,0 +1,128 @@
+package quotecache
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Quote : Stored response from the quoteserver
+type Quote struct {
+	UserID    string
+	Stock     string
+	Price     float64
+	Timestamp time.Time
+	Cryptokey string
+}
+
+// IsExpired : True if the quotes timestamp is older than its validity window
+func (q Quote) IsExpired() bool {
+	expiry := q.Timestamp.Add(time.Second * 45)
+	return time.Now().After(expiry)
+}
+
+// Global to store cached responses.
+// Maps stock name -> Quote
+var cache = make(map[string]Quote)
+
+// GetQuote : Gets the current value of the stock, hitting the local cache if it can.
+func GetQuote(userID, stock string) (Quote, error) {
+	// check if the value is in cache
+	quote, found := cache[stock]
+
+	if !found || quote.IsExpired() {
+		err := updateQuoteCache(userID, stock)
+		if err != nil {
+			return Quote{}, err
+		}
+
+		// assign the refreshed value
+		quote = cache[stock]
+	}
+
+	return quote, nil
+}
+
+// Refreshes the stock in the global quote cache
+func updateQuoteCache(userID, stock string) error {
+	conn, err := net.DialTimeout("tcp", getQuoteServAddress(), time.Second*10)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// Send that request!
+	request := fmt.Sprintf("%s,%s", stock, userID)
+	conn.Write([]byte(request))
+
+	// listen for response
+	message, err := bufio.NewReader(conn).ReadString('\n')
+	// when stream is done an EOF is omitted that we should ignore
+	if err != nil && err != io.EOF {
+		errMessage := fmt.Sprint("Bufio reader says:", err.Error())
+		return errors.New(errMessage)
+	}
+
+	// Convert the raw response to a Quote
+	quote, err := parseQuote(message)
+	if err != nil {
+		return err
+	}
+
+	cache[stock] = quote
+
+	return nil
+}
+
+// Returns the appropriate URL & Port based on the run environment.
+// Conrolled via environment flags
+func getQuoteServAddress() string {
+	var address string
+
+	switch os.Getenv("ENV") {
+	case "PROD":
+		address = "quoteserve.seng.uvic.ca:4443"
+	case "DEV":
+	default:
+		address = "localhost:4443"
+	}
+
+	return address
+}
+
+func parseQuote(s string) (Quote, error) {
+	parts := strings.Split(s, ",")
+
+	// Does the response have all the parts we need?
+	if len(parts) != 5 {
+		return Quote{}, errors.New("Incorrect number of fields returned by quoteserver")
+	}
+
+	// Convert string values from response to proper types
+	price, err := strconv.ParseFloat(parts[0], 64)
+	if err != nil {
+		return Quote{}, err
+	}
+
+	// Unix time has to be converted string -> int -> Time
+	unixTimeInt, err := strconv.ParseInt(parts[3], 10, 64)
+	if err != nil {
+		return Quote{}, err
+	}
+
+	quote := Quote{
+		Price:     price,
+		Stock:     parts[1],
+		UserID:    parts[2],
+		Timestamp: time.Unix(unixTimeInt, 0),
+		Cryptokey: parts[4],
+	}
+
+	return quote, nil
+}
