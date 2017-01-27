e4de77748456a394135241be905850ebda8a19e7
Tyler Stephen <tyler.stephen@gmail.com>
2017-01-27 00:07:15 -0800

    Log the actual time in ms
    - Extending sec by 1000 makes time jump by 1s in the logs. Truncating
      nanoseconds gives actual millisecond measurements.

--- auditlogger/auditlogger.go
+++ auditlogger/auditlogger.go
@@ -131,7 +131,7 @@ func LogCommand(cmd commands.Command) {
 		}
 	}
 
-	timeInMillisec := time.Now().Unix() * 1000
+	timeInMillisec := time.Now().UnixNano() / 1000
 
 	xmlElement := fmt.Sprintf(`
 	<userCommand>
